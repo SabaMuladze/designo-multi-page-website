5f379b3a96257c83c0da6ed48a7cd7cbd97e0a5c
SabaMuladze <muladze66@bk.ru>
2024-08-23 14:43:23 +0400

    hamburger

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="flex justify-between">
+  <header class="flex justify-between pb-5">
     <div>
       <img
         class="w-[200px]"
@@ -7,15 +7,31 @@
         alt=""
       />
     </div>
-    <div>
-      <img src="/assets/shared/mobile/icon-hamburger.svg" alt="" />
+    <div @click="toggleHam">
+      <img
+        v-if="hamburger"
+        src="/assets/shared/mobile/icon-hamburger.svg"
+        alt=""
+      />
+      <img v-else src="/assets/shared/mobile/icon-close.svg" alt="" />
     </div>
   </header>
 </template>
 <script>
 export default {
   data() {
-    return {};
+    return {
+      hamburger: true,
+    };
+  },
+  methods: {
+    toggleHam() {
+      if (this.hamburger == true) {
+        this.hamburger = false;
+      } else {
+        this.hamburger = true;
+      }
+    },
   },
 };
 </script>
