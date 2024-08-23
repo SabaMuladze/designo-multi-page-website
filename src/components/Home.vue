9213efab0c5b48ee19081c4798623cb9e9719ac8
SabaMuladze <muladze66@bk.ru>
2024-08-23 17:16:32 +0400

    peach seaction for tablet

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,11 @@
 <template>
-  <div>
+  <div class="md:p-6 md:pt-0">
     <div
-      class="bg-peach h-[843px] flex flex-col items-center overflow-hidden relative"
+      class="bg-peach h-[843px] flex flex-col items-center overflow-hidden relative md:rounded-lg"
     >
-      <div class="text-center p-4 flex flex-col gap-6 items-center pt-[60px]">
+      <div
+        class="text-center max-w-[573px] p-4 flex flex-col gap-6 items-center pt-[60px]"
+      >
         <h1 class="text-white max-sm:text-[32px] max-sm:leading-[36px]">
           Award-winning custom designs and digital branding solutions
         </h1>
@@ -16,7 +18,7 @@
           LEARN MORE
         </button>
       </div>
-      <div class="absolute bottom-[-120px]">
+      <div class="absolute bottom-[-120px] md:bottom-[-100px]">
         <img class="" src="/public/assets/home/desktop/phone.svg" alt="" />
       </div>
     </div>
