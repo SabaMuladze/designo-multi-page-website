e71f2b1a4a27b0f0a924b071dcf0469c98962eb3
SabaMuladze <muladze66@bk.ru>
2024-08-26 22:05:45 +0400

    page webdesign is done

--- src/components/WebDesign.vue
+++ src/components/WebDesign.vue
@@ -46,13 +46,39 @@
       ></web-design-card>
     </div>
   </section>
+  <section class="designs">
+    <div
+      class="flex flex-col gap-4 py-20 px-6 items-center justify-center lg:flex-row lg:px-0"
+    >
+      <div class="flex flex-col gap-4 lg:flex-row">
+        <router-link to="/appdesign">
+          <design-card
+            title="APP DESIGN"
+            mobpic="/assets/home/mobile/image-app-design.jpg"
+            tabpic="/assets/home/tablet/image-app-design.jpg"
+            despic="/assets/home/desktop/image-app-design.jpg"
+          ></design-card>
+        </router-link>
+        <router-link to="/graphic design">
+          <design-card
+            title="GRAPHIC DESIGN"
+            mobpic="/assets/home/mobile/image-graphic-design.jpg"
+            tabpic="/assets/home/tablet/image-graphic-design.jpg"
+            despic="/assets/home/desktop/image-graphic-design.jpg"
+          ></design-card>
+        </router-link>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import DesignCard from "./additional/DesignCard.vue";
 import WebDesignCard from "./additional/WebDesignCard.vue";
 export default {
   components: {
     WebDesignCard,
+    DesignCard,
   },
 };
 </script>
