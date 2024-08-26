8a77839fa840f13b101f0b217759bc6ed1214440
SabaMuladze <muladze66@bk.ru>
2024-08-26 17:25:56 +0400

    peach div for webdesign page

--- src/components/WebDesign.vue
+++ src/components/WebDesign.vue
@@ -1,8 +1,13 @@
 <template>
-  <section class="peach">
-    <div>
-      <h1>Web Design</h1>
-      <p></p>
+  <section class="peach flex justify-center">
+    <div
+      class="h-[320px] w-full max-w-[1111px] md:h-[252px] gap-5 bg-peach flex flex-col justify-center items-center text-center sm:rounded-lg sm:m-6"
+    >
+      <h1 class="text-white">Web Design</h1>
+      <p class="text-white max-w-[400px]">
+        We build websites that serve as powerful marketing tools and bring
+        memorable brand experiences.
+      </p>
     </div>
   </section>
 </template>
