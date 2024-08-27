d0e7dc2ef1bf704f0ea62aa01ad6ed2f626d6035
SabaMuladze <muladze66@bk.ru>
2024-08-27 17:37:39 +0400

    make peach section reusable for design pages

--- src/components/WebDesign.vue
+++ src/components/WebDesign.vue
@@ -1,15 +1,11 @@
 <template>
-  <section class="peach flex justify-center">
-    <div
-      class="h-[320px] w-full max-w-[1111px] md:h-[252px] gap-5 bg-peach flex flex-col justify-center items-center text-center sm:rounded-lg sm:m-6"
+  <PeachSecDesign>
+    <template #header>Web Design</template>
+    <template #body
+      >We build websites that serve as powerful marketing tools and bring
+      memorable brand experiences.</template
     >
-      <h1 class="text-white">Web Design</h1>
-      <p class="text-white max-w-[400px]">
-        We build websites that serve as powerful marketing tools and bring
-        memorable brand experiences.
-      </p>
-    </div>
-  </section>
+  </PeachSecDesign>
   <section class="design cards">
     <div
       class="flex py-20 max-w-[1111px] m-auto flex-wrap justify-center gap-6 p-6 md:flex-col md:items-center lg:flex-wrap lg:flex-row lg:px-0 lg:justify-between lg:py-[100px]"
@@ -75,10 +71,12 @@
 <script>
 import DesignCard from "./additional/DesignCard.vue";
 import WebDesignCard from "./additional/WebDesignCard.vue";
+import PeachSecDesign from "./additional/PeachSecDesign.vue";
 export default {
   components: {
     WebDesignCard,
     DesignCard,
+    PeachSecDesign,
   },
 };
 </script>
---
+++ src/components/additional/PeachSecDesign.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="peach flex justify-center">
+    <div
+      class="h-[320px] w-full max-w-[1111px] md:h-[252px] gap-5 bg-peach flex flex-col justify-center items-center text-center sm:rounded-lg sm:m-6"
+    >
+      <h1 class="text-white">
+        <slot name="header"></slot>
+      </h1>
+      <p class="text-white max-w-[400px]">
+        <slot name="body"></slot>
+      </p>
+    </div>
+  </section>
+</template>
