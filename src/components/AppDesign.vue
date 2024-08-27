705fc925df0167dcdb8250b7972d4912aa347aa2
SabaMuladze <muladze66@bk.ru>
2024-08-27 17:53:40 +0400

    design cards seciton done for appdesign page

--- src/components/AppDesign.vue
+++ src/components/AppDesign.vue
@@ -6,13 +6,46 @@
       right at their fingertips.</template
     >
   </peach-sec-design>
+  <section class="design cards">
+    <div
+      class="flex py-20 max-w-[1111px] m-auto flex-wrap justify-center gap-6 p-6 md:flex-col md:items-center lg:flex-wrap lg:flex-row lg:px-0 lg:justify-between lg:py-[100px]"
+    >
+      <web-design-card
+        pic="/assets/app-design/desktop/image-airfilter.jpg"
+        title="AIRFILTER"
+        desc="Solving the problem of poor indoor air quality by filtering the air"
+      ></web-design-card>
+      <web-design-card
+        pic="/assets/app-design/desktop/image-eyecam.jpg"
+        title="EYECAM"
+        desc="Product that lets you edit your favorite photos and videos at any time"
+      ></web-design-card>
+      <web-design-card
+        pic="/assets/app-design/desktop/image-faceit.jpg"
+        title="FACEIT"
+        desc="Get to meet your favorite internet superstar with the faceit app"
+      ></web-design-card>
+      <web-design-card
+        pic="/assets/app-design/desktop/image-loopstudios.jpg"
+        title="LOOPSTUDIOS"
+        desc="A VR experience app made for Loopstudios"
+      ></web-design-card>
+      <web-design-card
+        pic="/assets/app-design/desktop/image-todo.jpg"
+        title="TODO"
+        desc="A todo app that features cloud sync with light and dark mode"
+      ></web-design-card>
+    </div>
+  </section>
 </template>
 
 <script>
 import PeachSecDesign from "./additional/PeachSecDesign.vue";
+import WebDesignCard from "./additional/WebDesignCard.vue";
 export default {
   components: {
     PeachSecDesign,
+    WebDesignCard,
   },
 };
 </script>
