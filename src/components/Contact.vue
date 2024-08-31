b4d3d1c763e22a0ffd104d274d4f7b86a1dc4acb
SabaMuladze <muladze66@bk.ru>
2024-08-31 19:47:40 +0400

    reorganize contact page

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,42 +1,39 @@
 <template>
-  <peach-sec-design>
-    <template #header>Contact</template>
-    <template #body
-      >Ready to take it to the next level? Let’s talk about your project or idea
-      and find out how we can help your business grow. If you are looking for
-      unique digital experiences that’s relatable to your users, drop us a
-      line.</template
+  <div
+    class="w-full h-full flex flex-col justify-center items-center lg:flex-row lg:h-[480px]"
+  >
+    <div
+      class="px-6 bg-peach max-w-[1111px] m-auto mx-6 mt-[-5px] lg:flex-1 md:m-0 lg:h-[480px] lg:flex lg:items-center"
     >
-  </peach-sec-design>
-  <!-- <section class="bg-peach px-6">
-    <form>
-      <input
-        class="bg-transparent border-b-[1px] border-solid border-white py-3 w-full text-white placeholder:text-white placeholder:opacity-70 outline-none"
-        type="text"
-        placeholder="Name"
-      />
-      <input
-        class="bg-transparent border-b-[1px] border-solid border-white py-3 w-full text-white placeholder:text-white placeholder:opacity-70 outline-none"
-        type="email"
-        placeholder="Email Adress"
-      />
-      <input
-        class="bg-transparent border-b-[1px] border-solid border-white py-3 w-full text-white placeholder:text-white placeholder:opacity-70 outline-none"
-        type="number"
-        placeholder="Phone"
-      />
-      <textarea
-        class="bg-transparent w-full placeholder:text-white placeholder:opacity-70 outline-none border-b-[1px] border-solid border-white"
-        name="Message"
-        cols="20"
-        rows="10"
-        placeholder="Your Message"
-      ></textarea>
-      <div class="flex justify-center p-3">
-        <Button> Submit </Button>
-      </div>
-    </form>
-  </section> -->
+      <form>
+        <input
+          class="bg-transparent border-b-[1px] border-solid border-white py-3 w-full text-white placeholder:text-white placeholder:opacity-70 outline-none"
+          type="text"
+          placeholder="Name"
+        />
+        <input
+          class="bg-transparent border-b-[1px] border-solid border-white py-3 w-full text-white placeholder:text-white placeholder:opacity-70 outline-none"
+          type="email"
+          placeholder="Email Adress"
+        />
+        <input
+          class="bg-transparent border-b-[1px] border-solid border-white py-3 w-full text-white placeholder:text-white placeholder:opacity-70 outline-none"
+          type="number"
+          placeholder="Phone"
+        />
+        <textarea
+          class="bg-transparent w-full placeholder:text-white placeholder:opacity-70 outline-none border-b-[1px] border-solid border-white max-h-[100px]"
+          name="Message"
+          cols="10"
+          rows="10"
+          placeholder="Your Message"
+        ></textarea>
+        <div class="flex justify-center p-3">
+          <Button> Submit </Button>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
