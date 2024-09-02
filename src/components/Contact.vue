87f93dca5c9b172b2b02624b937b4edb0aba99bd
SabaMuladze <muladze66@bk.ru>
2024-09-02 22:07:10 +0400

    style:contact peach section on desktop

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -3,18 +3,20 @@
     class="w-full h-full flex flex-col justify-center items-center lg:flex-row lg:h-[480px] md:px-6"
   >
     <div
-      class="p-6 py-20 h-[764px] flex flex-col justify-between gap-10 bg-peach max-w-[1111px] md:h-[711px] md:w-[690px] md:px-10 md:rounded-lg lg:flex-1 md:m-0 lg:h-[480px] lg:flex-row lg:items-center"
+      class="p-6 py-20 h-[764px] flex flex-col justify-between gap-10 bg-peach max-w-[1111px] md:h-[711px] md:w-[690px] md:px-10 md:rounded-lg lg:flex-1 md:m-0 lg:h-[480px] lg:flex-row lg:items-center lg:px-20"
     >
-      <div class="flex flex-col gap-4 text-center md:text-start">
+      <div
+        class="flex flex-col gap-4 text-center md:text-start lg:flex-1 lg:gap-8 lg:justify-center"
+      >
         <h1 class="text-white max-sm:text-[32px]">Contact Us</h1>
-        <p class="text-white max-w-[400px]">
+        <p class="text-white max-w-[400px] lg:max-w-full">
           Ready to take it to the next level? Let’s talk about your project or
           idea and find out how we can help your business grow. If you are
           looking for unique digital experiences that’s relatable to your users,
           drop us a line.
         </p>
       </div>
-      <form>
+      <form class="flex-1">
         <input
           class="bg-transparent border-b-[1px] border-solid border-white py-3 w-full text-white placeholder:text-white placeholder:opacity-70 outline-none"
           type="text"
