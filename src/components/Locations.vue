e37936307aa2d8a76bf5d50879c94c153083168c
SabaMuladze <muladze66@bk.ru>
2024-09-03 22:52:54 +0400

    locations page for tablet

--- src/components/Locations.vue
+++ src/components/Locations.vue
@@ -1,19 +1,28 @@
 <template>
-  <section>
-    <div class="flex flex-col gap-10 pb-20">
-      <div>
+  <section class="flex flex-col items-center">
+    <div
+      class="flex flex-col gap-10 pb-20 md:px-6 sm:max-w-[690px] lg:max-w-[1111px]"
+    >
+      <div class="flex flex-col md:gap-5">
         <div>
           <img
-            class="w-full max-h-[326px]"
-            src="/public/assets/locations/desktop/image-map-canada.png"
+            class="w-full h-[326px] md:rounded-lg md:hidden lg:visible"
+            src="
+                /public/assets/locations/desktop/image-map-canada.png
+            "
+            alt=""
+          />
+          <img
+            class="w-full h-[326px] md:rounded-lg max-md:hidden lg:hidden"
+            src="/public/assets/locations/tablet/image-map-canada.png"
             alt=""
           />
         </div>
         <div
-          class="bg-[#FDF3F0] flex flex-col gap-5 items-center max-sm:text-center py-10"
+          class="bg-[#FDF3F0] flex flex-col gap-5 items-center max-sm:text-center py-10 md:rounded-lg md:flex-row md:pl-16 md:gap-[120px] md:items-center h-[326px]"
         >
           <div>
-            <h2 class="text-peach max-sm:text-[32px]">Canada</h2>
+            <h2 class="text-peach max-sm:text-[32px] mb-5">Canada</h2>
             <strong>Designo Central Office</strong>
             <p>3886 Wellington Street</p>
             <p>Toronto, Ontario M9C 3J5</p>
@@ -25,19 +34,24 @@
           </div>
         </div>
       </div>
-      <div>
+      <div class="flex flex-col md:gap-5">
         <div>
           <img
-            class="w-full max-h-[326px]"
+            class="w-full h-[326px] md:rounded-lg md:hidden lg:visible"
             src="/public/assets/locations/desktop/image-map-australia.png"
+            alt=""
+          />
+          <img
+            class="w-full h-[326px] md:rounded-lg max-md:hidden lg:hidden"
+            src="/public/assets/locations/tablet/image-map-canada.png"
             alt=""
           />
         </div>
         <div
-          class="bg-[#FDF3F0] flex flex-col gap-5 items-center max-sm:text-center py-10"
+          class="bg-[#FDF3F0] flex flex-col gap-5 items-center max-sm:text-center py-10 md:rounded-lg md:flex-row md:pl-16 md:gap-[120px] md:items-center h-[326px]"
         >
           <div>
-            <h2 class="text-peach max-sm:text-[32px]">Australia</h2>
+            <h2 class="text-peach max-sm:text-[32px] mb-5">Australia</h2>
             <strong>Designo AU Office</strong>
             <p>19 Balonne Street</p>
             <p>New South Wales 2443</p>
@@ -49,19 +63,24 @@
           </div>
         </div>
       </div>
-      <div>
+      <div class="flex flex-col md:gap-5">
         <div>
           <img
-            class="w-full max-h-[326px]"
+            class="w-full h-[326px] md:rounded-lg md:hidden lg:visible"
             src="/public/assets/locations/desktop/image-map-united-kingdom.png"
+            alt=""
+          />
+          <img
+            class="w-full h-[326px] md:rounded-lg max-md:hidden lg:hidden"
+            src="/public/assets/locations/tablet/image-map-canada.png"
             alt=""
           />
         </div>
         <div
-          class="bg-[#FDF3F0] flex flex-col gap-5 items-center max-sm:text-center py-10"
+          class="bg-[#FDF3F0] flex flex-col gap-5 items-center max-sm:text-center py-10 md:rounded-lg md:flex-row md:pl-16 h-[326px]"
         >
           <div>
-            <h2 class="text-peach max-sm:text-[32px]">United Kingdom</h2>
+            <h2 class="text-peach max-sm:text-[32px] mb-5">United Kingdom</h2>
             <strong>Designo UK Office</strong>
             <p>13 Colorado Way</p>
             <p>Rhyd-y-fro SA8 9GA</p>
@@ -77,6 +96,18 @@
   </section>
 </template>
 
-<script></script>
+<script>
+export default {
+  computed: {
+    changeLocationImg() {
+      if (window.innerWidth < 768 || window.innerWidth > 1024) {
+        return desktop;
+      } else {
+        return tablet;
+      }
+    },
+  },
+};
+</script>
 
 <style scoped></style>
