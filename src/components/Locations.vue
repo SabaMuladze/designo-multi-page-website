c71264a24b5458f569f90ef66b1fd271b1d22812
SabaMuladze <muladze66@bk.ru>
2024-09-03 21:45:30 +0400

    start working on locations page

--- src/components/Locations.vue
+++ src/components/Locations.vue
@@ -1,5 +1,32 @@
 <template>
-  <h1>Locations</h1>
+  <section>
+    <div class="flex flex-col">
+      <div>
+        <div>
+          <img
+            src="/public/assets/locations/tablet/image-map-canada.png"
+            alt=""
+          />
+        </div>
+        <div class="bg-[#FDF3F0]"></div>
+      </div>
+      <div>
+        <div>
+          <img
+            src="/public/assets/locations/tablet/image-map-australia.png"
+            alt=""
+          />
+        </div>
+        <div class="bg-[#FDF3F0]"></div>
+      </div>
+      <div>
+        <div>
+          <img src="/public/assets/locations/tablet/image-map-uk.png" alt="" />
+        </div>
+        <div class="bg-[#FDF3F0]"></div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script></script>
