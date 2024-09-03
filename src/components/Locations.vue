c09f1b6fc8a29780a112ed505e2ec70cac495f51
SabaMuladze <muladze66@bk.ru>
2024-09-03 21:58:35 +0400

    styles:locations on mobile

--- src/components/Locations.vue
+++ src/components/Locations.vue
@@ -8,7 +8,21 @@
             alt=""
           />
         </div>
-        <div class="bg-[#FDF3F0]"></div>
+        <div
+          class="bg-[#FDF3F0] flex flex-col gap-5 items-center max-sm:text-center"
+        >
+          <div>
+            <h2 class="text-peach max-sm:text-[32px]">Canada</h2>
+            <strong>Designo Central Office</strong>
+            <p>3886 Wellington Street</p>
+            <p>Toronto, Ontario M9C 3J5</p>
+          </div>
+          <div>
+            <strong>Contact</strong>
+            <p>P : +1 253-863-8967</p>
+            <p>M : contact@designo.co</p>
+          </div>
+        </div>
       </div>
       <div>
         <div>
@@ -17,13 +31,41 @@
             alt=""
           />
         </div>
-        <div class="bg-[#FDF3F0]"></div>
+        <div
+          class="bg-[#FDF3F0] flex flex-col gap-5 items-center max-sm:text-center"
+        >
+          <div>
+            <h2 class="text-peach max-sm:text-[32px]">Australia</h2>
+            <strong>Designo AU Office</strong>
+            <p>19 Balonne Street</p>
+            <p>New South Wales 2443</p>
+          </div>
+          <div>
+            <strong>Contact</strong>
+            <p>P : (02) 6720 9092</p>
+            <p>M : contact@designo.au</p>
+          </div>
+        </div>
       </div>
       <div>
         <div>
           <img src="/public/assets/locations/tablet/image-map-uk.png" alt="" />
         </div>
-        <div class="bg-[#FDF3F0]"></div>
+        <div
+          class="bg-[#FDF3F0] flex flex-col gap-5 items-center max-sm:text-center"
+        >
+          <div>
+            <h2 class="text-peach max-sm:text-[32px]">United Kingdom</h2>
+            <strong>Designo UK Office</strong>
+            <p>13 Colorado Way</p>
+            <p>Rhyd-y-fro SA8 9GA</p>
+          </div>
+          <div>
+            <strong>Contact</strong>
+            <p>P : 078 3115 1400</p>
+            <p>M : contact@designo.uk</p>
+          </div>
+        </div>
       </div>
     </div>
   </section>
