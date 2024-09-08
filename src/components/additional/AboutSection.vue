20d4820b7e38f07b95509bfb667feae85e41dd4b
SabaMuladze <muladze66@bk.ru>
2024-09-08 21:11:49 +0400

    responsive for tablet

--- src/components/additional/AboutSection.vue
+++ src/components/additional/AboutSection.vue
@@ -1,18 +1,33 @@
 <template>
   <section>
-    <div :class="`w-full flex justify-center max-lg:text-center  `">
-      <div :class="'max-w-[690px] lg:max-w-[1111px]' + textCol + ' ' + bgCol">
+    <div :class="`w-full flex justify-center sm:px-6 max-lg:text-center `">
+      <div
+        :class="
+          'flex flex-col w-full gap-10 sm:max-w-[690px]  sm:rounded-lg pb-10 lg:max-w-[1111px] ' +
+          textCol +
+          ' ' +
+          bgCol
+        "
+      >
         <div>
-          <img class="md:hidden w-full h-[320px]" :src="mobpic" alt="" />
           <img
-            class="max-md:hidden lg:hidden w-full h-[320px]"
+            class="md:hidden sm:rounded-t-lg w-full h-[320px]"
+            :src="mobpic"
+            alt=""
+          />
+          <img
+            class="max-md:hidden sm:rounded-t-lg lg:hidden w-full h-[320px]"
             :src="tabpic"
             alt=""
           />
-          <img class="max-lg:hidden w-full" :src="despic" alt="" />
+          <img
+            class="max-lg:hidden sm:rounded-lg w-full h-[480px]"
+            :src="despic"
+            alt=""
+          />
         </div>
-        <div>
-          <h1 :class="'' + headCol">{{ header }}</h1>
+        <div class="flex flex-col gap-5 md:px-10 px-6">
+          <h1 :class="'max-sm:text-[32px] ' + headCol">{{ header }}</h1>
           <p :class="textCol">{{ desc }}</p>
         </div>
       </div>
