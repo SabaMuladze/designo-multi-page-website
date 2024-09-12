9a84434bb1e8626a9c4aa29c6fbd87cefe2345b9
SabaMuladze <muladze66@bk.ru>
2024-09-12 22:16:15 +0400

    navmobile closes after changing router path

--- src/components/NavMobile.vue
+++ src/components/NavMobile.vue
@@ -1,21 +1,21 @@
 <template>
   <div
     v-if="hamburger == true"
-    class="absolute z-50 bg-black bg-opacity-50 w-full min-h-[100vh]"
+    class="absolute z-50 bg-black bg-opacity-50 w-full h-full sm:hidden"
   >
     <div class="bg-black p-10">
       <ul class="flex flex-col gap-8">
-        <li>
+        <li @click="toggleHam">
           <router-link to="/about"
             ><h3 class="text-white font-medium">OUR COMPANY</h3></router-link
           >
         </li>
-        <li>
+        <li @click="toggleHam">
           <router-link to="/locations"
             ><h3 class="text-white font-medium">LOCATIONS</h3></router-link
           >
         </li>
-        <li>
+        <li @click="toggleHam">
           <router-link to="/contact"
             ><h3 class="text-white font-medium">CONTACT</h3></router-link
           >
@@ -26,10 +26,13 @@
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import { mapGetters, mapActions } from "vuex";
 export default {
   computed: {
     ...mapGetters(["hamburger"]),
+  },
+  methods: {
+    ...mapActions(["toggleHam"]),
   },
 };
 </script>
